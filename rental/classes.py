6b005b8fc26e83586b52b01572366b55ab1f2e65
FernandoGomesK <fernandogomest94@gmail.com>
2025-04-09 11:52:48 -0300

    Inheritance

--- rental/classes.py
+++ rental/classes.py
@@ -6,6 +6,16 @@ class Rental:
         self.__name = name
         self.__branches = []
         
+    ###########getters and setters##############
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    ############Methods################################
     def add_branch(self, branch):
         self.__branches.append(branch)
         
@@ -19,6 +29,39 @@ class Worker:
         self.__cpf = cpf
         self.__age = age
         self.__function = function
+    ###########getters and setters##############
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+        
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+        
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, age):
+        self.__age = age
+        
+    @property
+    def function(self):
+        return self.__function
+    
+    @function.setter
+    def function(self, function):
+        self.__function = function
+    ############Methods################################
     
 class Branch:
     def __init__(self, name, location):
@@ -43,7 +86,7 @@ class Branch:
         
     def list_vehicles(self):
         for vehicle in self.__vehicles:
-            print(vehicle.car_info())
+            print(vehicle.vehicle_info())
         
 class Client:   
     def __init__(self, name, cpf, age):
@@ -65,8 +108,18 @@ class Vehicle:
         self.__base_price = base_price
         self.__mileage_price = mileage_price
         
-    def car_info(self):
+    def vehicle_info(self):
         return f"{self.__brand} {self.__model} ({self.__year}) plate: {self.__plate}"
+    
+class Car(Vehicle):
+    def __init__(self, brand, plate, model, year, base_price, mileage_price):   
+        super().__init__(brand, plate, model, year, base_price, mileage_price)
+        
+        
+class Motorcycle(Vehicle):
+    def __init__(self, brand, plate, model, year, base_price, mileage_price, cubic_capacity):
+        self.__cubic_capacity = cubic_capacity
+        super().__init__(brand, plate, model, year, base_price, mileage_price)
 
 class Location:
     def __init__(self, client, vehicle, start_date, end_date):
@@ -115,12 +168,14 @@ branch1.add_client(jonas)
 
 branch1.list_clients()
 
-supra1 = Vehicle("Toyota", "br1", "Supra Mk4", 1995, 20, 20)
-celta1 = Vehicle("Chevrolet", "br2", "Celta", 2011, 10, 15)
-altima = Vehicle("Nissan", "Br3", "Altima", 2025, 30, 25)
+supra1 = Car("Toyota", "br1", "Supra Mk4", 1995, 20, 20)
+celta1 = Car("Chevrolet", "br2", "Celta", 2011, 10, 15)
+altima = Car("Nissan", "Br3", "Altima", 2025, 30, 25)
+ninja = Motorcycle("Kawasaki", "jp1", "ninja", 2011, 20, 10, 250)
 branch1.add_vehicle(celta1)
 branch1.add_vehicle(supra1)
 branch1.add_vehicle(altima)
+branch1.add_vehicle(ninja)
 
 branch1.list_vehicles() 
 
